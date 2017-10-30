1c872c82d7cb24e616e8dd31bb813341b643452d
ashBose <bose.aishwarya.0210@gmail.com>
2017-10-30 12:14:51 -0700

    Update Analytics.py

--- president/Analytics.py
+++ president/Analytics.py
@@ -27,9 +27,9 @@ class Analytics:
 			self.new_df, return_type="dataframe")
 		y = np.ravel(y)
 		X_train, X_test, y_train, y_test = train_test_split(X,
-															y,
-															test_size=0.3,
-															random_state=0)
+									y,
+									test_size=0.3,
+									random_state=0)
 		return X, y, X_train, X_test, y_train, y_test
 
 	def logistic_model(self, X_train, y_train, X_test):
